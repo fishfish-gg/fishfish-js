de5ecd96c0052ef8571aedb426065eb61e7d03a7
JPBM135 <jpedrobm0@gmail.com>
2022-10-28 23:17:34 -0300

    refactor(*): move to a `structures` folder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './constants.js';
 export * from './errors.js';
 export * from './types.js';
-export * from './api.js';
+export * from './structures/api.js';
+export * from './structures/auth.js';
+export * from './structures/webSocket.js';
